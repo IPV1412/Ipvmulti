6c2d12db38356c444043bae2e20e545846177984
IPV1412 <jonathan.nava@uniat.edu.mx>
2025-05-09 02:50:07 -0600

    Aro fallguys

---
+++ Source/Ipvmulti/Private/Actors/Aro_v2.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/Aro_v2.h"
+
+// Sets default values
+AAro_v2::AAro_v2()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AAro_v2::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAro_v2::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
